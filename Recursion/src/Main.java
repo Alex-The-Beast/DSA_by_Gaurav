4e9cb6f8d10e4450967306286bc729c968ac9fa2
Gaurav kumar <gk064577@gmail.com>
2025-05-30 19:33:45 +0530

    Added the first question of recursion

--- Recursion/src/Main.java
+++ Recursion/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static void printIncreasing(int n){
+        if (n==1){
+            System.out.println(n);
+            return ;
+        }
+        printIncreasing(n-1);
+        System.out.println(n);
+    }
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        printIncreasing(n);
+
         System.out.println("Hello, World!");
     }
 }

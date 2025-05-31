799b88f0ce2eafd72244df7b02bdd5de54dba1cf
Gaurav kumar <gk064577@gmail.com>
2025-05-31 12:45:50 +0530

    find gcd using recursive method

---
+++ Recursion/src/GCDRecursive.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GCDRecursive {
+    static int gcd(int x,int y){
+        if (x < 0 || y < 0) throw new IllegalArgumentException("GCD requires non-negative integers");
+        if (y==0) return x;
+        return gcd(y,x%y);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        System.out.println( gcd(x,y));
+    }
+}

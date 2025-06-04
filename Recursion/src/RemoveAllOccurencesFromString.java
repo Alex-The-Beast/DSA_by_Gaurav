e050891ea6b14341d2b18c1a5ad619dd19016f2d
Gaurav kumar <gk064577@gmail.com>
2025-06-04 21:19:44 +0530

    add substring method.

--- Recursion/src/RemoveAllOccurencesFromString.java
+++ Recursion/src/RemoveAllOccurencesFromString.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
 public class RemoveAllOccurencesFromString {
+
+    static String Remove2 (String s){
+        if(s.length()==0) return "";
+        String smallAns=Remove2(s.substring(1));
+        char currChar=s.charAt(0);
+        if(currChar!='a'){
+            return  currChar+smallAns;
+        }else{
+            return smallAns;
+        }
+    }
     static String RemoveOccurence(String s,int idx,String target){
         //base case
-           if(idx>=s.length()) return "";
+        if(idx>=s.length()) return "";
         //recursive work
-           String ans=RemoveOccurence(s,idx+1,target);
+        String ans=RemoveOccurence(s,idx+1,target);
         //self work
-           if(s.charAt(idx)!=target.charAt(0)){
-               return s.charAt(idx)+ans;
-           }else return ans;
+        if(s.charAt(idx)!=target.charAt(0)){
+            return s.charAt(idx)+ans;
+        }else return ans;
 
     }
     public static void main(String[] args) {
@@ -21,5 +32,6 @@ public class RemoveAllOccurencesFromString {
         String a=sc.nextLine();
         String result=RemoveOccurence(s,0,a);
         System.out.println(result);
+        System.out.println("This comes from substring methond:"+ Remove2(s));
     }
 }
